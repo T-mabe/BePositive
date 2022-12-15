d934778d690068b215abb132d69c765c66e598d7
Abe Molina <abemolina03@gmail.com>
2022-12-15 15:27:28 -0500

    Initial Commit

---
+++ Be Positive/Be_PositiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Be_PositiveApp.swift
+//  Be Positive
+//
+//  Created by Abe Molina on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Be_PositiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

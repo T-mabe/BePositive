3e9f7dc293be671e12ff3a5f1fed1018fb7005d1
Abe Molina <abemolina03@gmail.com>
2023-03-01 11:55:08 -0500

    UI design change

--- Be Positive/HomeView.swift
+++ Be Positive/HomeView.swift
@@ -13,8 +13,10 @@ struct HomeView: View {
     @State private var showStory = false
     @State var tapped = false
     @State private var isAnimating = false
+    @State var buttonText = ""
+    @FocusState var isInputActive: Bool
+
    
-    
     var body: some View {
         
         ZStack {
@@ -24,26 +26,30 @@ struct HomeView: View {
                 
                 ZStack {
                     //                    Core Interface
-                    Color(eggShell)
+                    Color("color")
                         .edgesIgnoringSafeArea(.all)
                     
                     ZStack {
                         
-                        Image("EmptyTeeV")
-                            .resizable()
-                            .scaledToFit()
+//                        Image("EmptyTeeV")
+//                            .resizable()
+//                            .scaledToFit()
                         
                         Image("Sebby")
                             .resizable()
                             .scaledToFill()
-                            .frame(width: 175, height: 140 )
+                            .frame(width: 375, height: 540 )
                             .clipShape(RoundedRectangle(cornerRadius: 15))
-                            .offset(y: -76).offset(x: -25)
+//                            .offset(y: -76).offset(x: -25)
                             .onTapGesture {
                                 withAnimation {
                                     tapped.toggle()
                                 }
                             }.animation(.default, value: tapped)
+                        Spacer()
+                        
+                        
+                        
                         
                     }  .frame(width: 250)
                         .padding(.top, 180)
@@ -55,11 +61,11 @@ struct HomeView: View {
                             NavigationLink {
                                 ProfileView()
                             } label: {
-                                Image(systemName: "person.crop.circle.badge.plus")
+                                Image(systemName: "plus")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 85, height: 40)
-                                    .foregroundColor(.black)
+                                    .frame(width: 85, height: 25)
+                                    .foregroundColor(.primary)
                             }
                             
                             Spacer()
@@ -71,7 +77,7 @@ struct HomeView: View {
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 40, height: 40)
-                                .foregroundColor(.black)
+                                .foregroundColor(.primary)
                                 .padding()
                         }
                     }
@@ -79,20 +85,29 @@ struct HomeView: View {
                         //                        Heading Text
                         Text("Be")
                             .bold()
-                            .font(.system(size: 80))
+                            .font(.system(size: 60))
                             .multilineTextAlignment(.center)
-                            .padding(.top, 45)
-                            .foregroundColor(.black)
+//                            .padding(., 45)
+                            .foregroundColor(.primary)
                         
                         Text("Positive")
                             .bold()
                             .foregroundColor(.red)
-                            .font(.system(size: 80))
+                            .font(.system(size: 60))
+                            
                         
                         //                        Camera Icon with spacers to position
                         Spacer()
                         
                         HStack {
+                            TextField("Leave a comment", text: $buttonText)
+                                .foregroundColor(.primary)
+                                .font(.system(size: 23))
+                                .textFieldStyle(.automatic)
+//                                .multilineTextAlignment(.center)
+//                                .font(.headline)
+                                .padding()
+                                .focused($isInputActive)
                             
                             Spacer()
                             
@@ -102,11 +117,11 @@ struct HomeView: View {
                                 Image(systemName: "camera")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 65)
-                                    .foregroundColor(.black)
+                                    .frame(width: 45)
+                                    .foregroundColor(.primary)
                             }
                         }
-                        .padding()
+                        .padding(.horizontal, 30)
                     }
                     
                     //                    This needs to be cleaned up
